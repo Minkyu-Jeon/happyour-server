cf4889f84b9e80f63c414c79c9d9bf87c2640614
Minkyu Jeon <minkyu@huray.net>
2017-06-01 23:50:45 +0900

    set validates at user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
 
   has_many :invitations, inverse_of: :user
   has_many :orders, inverse_of: :user
+
+  validates :email, absence: true, uniqueness: true
+  validates :nickname, presence: true, uniqueness: true
+  validates :recommendation_code, length: { maximum: 10 }, uniqueness: true
+  validates :social_type, inclusion: { in: [1, 2] }, allow_nil: true
+
+  before_create :gen_recommendation_code
+
+  private
+  def gen_recommendation_code
+  	loop do
+  		code = SecureRandom.hex(10)
+  		self.recommendation_code = code
+  		break if User.find_by(recommendation_code: code).nil?
+  	end
+  end
 end
