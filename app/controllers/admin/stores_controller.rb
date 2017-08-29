b652a18b13bd9a50d16260ae89c2b1ce5617279e
Minkyu-Jeon <minkyu@huray.net>
2017-08-29 21:09:20 +0900

    [admin] 수정페이지 와꾸 추가

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -1,6 +1,16 @@
 class Admin::StoresController < Admin::BaseController
+  before_action :find_store, only: [:edit]
 	def index
 		set_menu("stores", "index")
 		@stores = Store.all
 	end
+
+  def edit
+    set_menu("stores", "edit")
+  end
+
+  private
+  def find_store
+    @store = Store.find(params[:id])
+  end
 end
