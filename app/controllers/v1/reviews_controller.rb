3006d4eef26191e740bac2a60640d8b2d1f64a36
Minkyu Jeon <minkyu@huray.net>
2018-02-17 11:52:30 +0900

    리뷰 조회에 페이징 추가

--- app/controllers/v1/reviews_controller.rb
+++ app/controllers/v1/reviews_controller.rb
@@ -1,14 +1,20 @@
 class V1::ReviewsController < ::ApiController
-  before_action :authenticate_user_token!, only: [:create]
-
+  before_action :authenticate_user_token!, only: [:create, :evaluate]
+  PER_PAGE = 15
   def index
     require_params! :current_dttm
-    @reviews = Store.find(params[:store_id]).
-      reviews.
-      where("reviews.created_at > ?", params[:current_dttm]).
-      includes(:user, :review_evaluations) # 일단 최신순
+    @reviews = Store.find(params[:store_id]).reviews
+    total_count = @reviews.count
+    page = params[:page] || 0
 
-    render json: @reviews, adapter: :json, root: :reviews
+    # 일단 최신순
+    @reviews = @reviews.
+      where("reviews.created_at < ?", params[:current_dttm]).
+      includes(:user, :review_evaluations).
+      limit(PER_PAGE).
+      offset(PER_PAGE * page)
+
+    render json: @reviews, adapter: :json, root: :reviews, meta: { per_page: PER_PAGE, total_count: total_count }
   end
 
   def create
