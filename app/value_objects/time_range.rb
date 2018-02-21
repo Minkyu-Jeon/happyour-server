d5093fb7d229fc16ee783d41fc39dbfe9f77d11d
Minkyu Jeon <minkyu@huray.net>
2018-02-21 20:42:20 +0900

    TimeRange 클래스에 validation 추가

--- app/value_objects/time_range.rb
+++ app/value_objects/time_range.rb
@@ -1,9 +1,10 @@
 class TimeRange
   attr_reader :start_time, :end_time
 
-  def initialize(start_time, end_time, is_12=false)
+  def initialize(start_time, end_time)
     @start_time, @end_time = start_time, end_time
-
+    valid_format?
+    valid_range?
   end
 
   def in_range?(t)
@@ -12,5 +13,22 @@ class TimeRange
 
   def to_s
     "#{start_time} ~ #{end_time}"
+  end
+
+  private
+  def valid_format?
+    regexp = /^\d{2}:\d{2}$/
+    regexp.match(start_time) or raise "start_time '#{start_time}' is invalid"
+    regexp.match(end_time) or raise "end_time '#{end_time}' is invalid"
+  end
+
+  def valid_range?
+    to_min = -> (time) { 
+      t = time.split(":") 
+      t[0].to_i * 60 + t[1].to_i
+    }
+    start_sec = to_min.call(start_time)
+    end_sec = to_min.call(end_time)
+    start_sec > end_sec and raise "end_time can't grater then start_time"
   end
 end
